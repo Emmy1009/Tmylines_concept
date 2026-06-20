577c3e2b3aa2fcf4638849ca6bdedd14e52bdd43
Emmy1009 <emmygoldadebayo@gmail.com>
2026-06-20 09:34:59 +0100

    The changing of domain

--- frontend/src/components/axios.js
+++ frontend/src/components/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://tmylines-00ep.onrender.com/api"
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://tmylines-ex4l.onrender.com"
 const instance = axios.create({
     baseURL: BASE_URL, 
     withCredentials: true
